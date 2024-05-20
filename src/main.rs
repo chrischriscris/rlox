54552942c230f3fc6c96a1648d1cf9fb4a83f5ea
chrischriscris <18-10892@usb.ve>
2024-05-19 20:53:54 -0400

    para pensar

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ mod value;
 
 fn main() -> Result<(), ()> {
     let mut chunk = Chunk::new();
+
+    let constant = chunk.add_constant(1.2);
+
+    chunk.write(OpCode::OpConstant);
     chunk.write(OpCode::OpReturn);
 
     disassemble_chunk(&chunk, "test chunk");
